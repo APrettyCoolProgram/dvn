d5543c65f5f7bbd46cd8096b4c6a05b893be35e2
Chris Banwarth <github@aprettycoolprogram.com>
2025-07-15 06:43:10 -0400

    Update ProjectInfo.cs

--- src/dvngui/ProjectInfo.cs
+++ src/dvngui/ProjectInfo.cs
@@ -1,15 +1,10 @@
 ﻿/* dvngui
  * Front end interface for dvn.
- * v0.0 (b0)
+ * v0.9.0 (b250710.1533)
  *
  * https://github.com/APrettyCoolProgram/dvn
  * Copyright (c) A Pretty Cool Program. All rights reserved.
  * Licensed under the Apache 2.0 license.
- */
-
-/* dvngui.ProjectInfo.cs
- * u250701_code
- * u250701_documentation
  */
 
 namespace dvngui
