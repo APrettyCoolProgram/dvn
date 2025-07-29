869e8da936341f61ca43bb9a74b546bcf0c41220
Chris Banwarth <github@aprettycoolprogram.com>
2025-07-29 10:38:26 -0400

    Repository resources

---
+++ .github/archived-src/src-0.9-250719/dvnlib/Du/DuFile.cs
@@ -0,0 +1,19 @@
+﻿/* DuFile.cs
+ * Does stuff with files.
+ * b250630
+ * A Pretty Cool Program
+ * https://gist.github.com/APrettyCoolProgram/6f8cb8e700fdccc39bf5314aefec8703
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dvnlib.Du
+{
+    public class DuFile
+    {
+    }
+}
